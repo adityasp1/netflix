8bcd2d02e60e84fac1f72758ff14d01b8a6487b2
Aditya Rawat <adityarawat2801@gmail.com>
2023-04-02 11:47:32 +0530

    movie updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,8 @@ const Footer = () => {
   return (
     <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
         <h1 className=' flex text-white underline items-center justify-center text-2xl font-bold cursor-pointer'>Build by Aditya</h1>
-      <div className='flex text-white m-6 px-3 py-3 cursor-pointer'>
-        <a className='pl-4' href="https://twitter.com/aditya_sp3" target='_blank'><TbBrandTwitter /></a>
+      <div className='flex text-white m-6 px-3 py-3 cursor-pointer '>
+        <a className='pl-4 ' href="https://twitter.com/aditya_sp3" target='_blank'><TbBrandTwitter /></a>
         
       <a className='pl-4' href="https://github.com/adityasp1" target='_blank'><TbBrandGithub /></a>
         
